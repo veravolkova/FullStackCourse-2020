21813a17693689870868ec4cfa7233b2524ba1df
veravolkova <verap87@mail.ru>
2020-05-08 10:29:38 +0300

    9.8-9.9

---
+++ part9/patientor/patientor-server/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+const app = express();
+
+import cors from "cors";
+
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => { 
+  console.log('someone pinged here');
+  res.send('pong');
+});
+  
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
