cd7ef570ae24322067062c7a730d80764c1aa4ad
veravolkova <verap87@mail.ru>
2020-07-24 11:01:20 +0300

    ex. 7.4 - 7.7

---
+++ part7/country-hook/src/hooks/index.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+export const useCountry = (name) => {
+    const [country, setCountry] = useState(null)
+
+    useEffect(() => {
+        try {
+            if (name) {                
+                axios
+                    .get(`https://restcountries.eu/rest/v2/name/${name}`)
+                    .then((response) => {
+                        //console.log(response.data[0])                                             
+                        setCountry(response.data[0])
+                    })
+            }
+        } catch (e) {
+            console.log("Error")
+        }
+    }, [name])
+
+    return country
+}
+
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    return {
+        type,
+        value,
+        onChange
+    }
+}
--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,41 +1,49 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router-dom'
+import  { useField } from '../hooks/index'
 
-const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
-   
+const CreateNew = (props) => {   
+    const { reset: resetContent, ...content } = useField('text')
+    const { reset: resetAuthor, ...author } = useField('text')
+    const { reset: resetInfo, ...info } = useField('text')  
+          
     const history = useHistory()
   
     const handleSubmit = (e) => {
       e.preventDefault()
       props.addNew({
-        content,
-        author,
-        info,
+        content: content.value,
+        author: author.value,
+        info: info.value,
         votes: 0
       })
       history.push('/')
     }
-  
+
+    const clearAll = () => {
+      resetContent()
+      resetAuthor()
+      resetInfo()
+    } 
+   
     return (
       <div>
         <h2>create a new anecdote</h2>
         <form onSubmit={handleSubmit}>
           <div>
             content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+            <input {...content} />            
           </div>
           <div>
             author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+            <input {...author} />             
           </div>
           <div>
             url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+            <input {...info} />          
           </div>
-          <button>create</button>
+          <button type="submit">create</button>
+          <button type="button" onClick={clearAll}>reset</button>
         </form>
       </div>
     )      
